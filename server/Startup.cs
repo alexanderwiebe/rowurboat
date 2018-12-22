bcfb648e2b7bed3219c7b80b1dbf4de9ef46479e
Alexander <alexanderjwiebe@gmail.com>
2018-12-21 23:30:56 -0600

    refactored to have the dotnet app have the api and websockets

--- server/Startup.cs
+++ server/Startup.cs
@@ -1,5 +1,4 @@
-﻿#define UseOptions // or NoOptions
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.WebSockets;
@@ -9,57 +8,62 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
-namespace EchoApp
+namespace TodoApi
 {
   public class Startup
   {
     public List<String> msgs = new List<String>(){
-            "The", "main", "difference", "between",
-          "altcoins", "and", "tokens", "is", "in",
-          "their", "structure;", "altcoins", "are",
-          "separate", "currencies", "with", "their",
-          "own", "separate", "blockchain", "while",
-          "tokens", "operate", "on", "top", "of",
-          "a", "blockchain", "that", "facilitates",
-          "the", "creation", "of", "decentralized", "applications."
-        };
+        "The", "main", "difference", "between",
+      "altcoins", "and", "tokens", "is", "in",
+      "their", "structure;", "altcoins", "are",
+      "separate", "currencies", "with", "their",
+      "own", "separate", "blockchain", "while",
+      "tokens", "operate", "on", "top", "of",
+      "a", "blockchain", "that", "facilitates",
+      "the", "creation", "of", "decentralized", "applications."
+    };
+
+
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
 
     // This method gets called by the runtime. Use this method to add services to the container.
-    // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
     {
-      loggerFactory.AddConsole(LogLevel.Debug);
-      loggerFactory.AddDebug(LogLevel.Debug);
-
       if (env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
       }
+      else
+      {
+        app.UseHsts();
+      }
 
-#if NoOptions
-      #region UseWebSockets
-            app.UseWebSockets();
-      #endregion
-#endif
-#if UseOptions
-      #region UseWebSocketsOptions
       var webSocketOptions = new WebSocketOptions()
       {
         KeepAliveInterval = TimeSpan.FromSeconds(120),
         ReceiveBufferSize = 4 * 1024,
       };
+      
       app.UseWebSockets(webSocketOptions);
-      #endregion
-#endif
-      #region AcceptWebSocket
+
       app.Use(async (context, next) =>
       {
         if (context.Request.Path == "/ws")
@@ -67,9 +71,9 @@ namespace EchoApp
           if (context.WebSockets.IsWebSocketRequest)
           {
             WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
-                  //await Echo(context, webSocket);
-                  //await Word(context, webSocket);
-                  await PushWord(context, webSocket);
+            //await Echo(context, webSocket);
+            //await Word(context, webSocket);
+            await PushWord(context, webSocket);
           }
           else
           {
@@ -82,29 +86,11 @@ namespace EchoApp
         }
 
       });
-      #endregion
-      app.UseFileServer();
-    }
-    #region Echo
-    /***
-     * sample of echoing the message
-     * */
-    private async Task Echo(HttpContext context, WebSocket webSocket)
-    {
-      var buffer = new byte[1024 * 4];
-      WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-      while (!result.CloseStatus.HasValue)
-      {
-        await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
 
-        result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-      }
-      await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+      app.UseHttpsRedirection();
+      app.UseMvc();
     }
-    #endregion
-    /***
-     * returns the next word
-     * */
+
     private async Task Word(HttpContext context, WebSocket webSocket)
     {
       var buffer = new byte[1024 * 4];
