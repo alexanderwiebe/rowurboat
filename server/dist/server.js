6ef1ebbfa19e8b36d12f77487466dc4c42a344eb
Alexander <alexanderjwiebe@gmail.com>
2018-12-28 14:44:28 -0600

    separate files for ws stuff.  added usb and csafe dependencies

--- server/dist/server.js
+++ server/dist/server.js
@@ -3,10 +3,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const WebSocket = require('ws');
 const app_1 = __importDefault(require("./app"));
 const server = app_1.default.listen(app_1.default.get("port"), () => {
     console.log("  App is running at http://localhost:%d in %s mode", app_1.default.get("port"), app_1.default.get("env"));
     console.log("  Press CTRL-C to stop\n");
+});
+const ws = new WebSocket('ws://localhost:7331/');
+ws.on('open', function open() {
+    ws.send('something');
+});
+ws.on('message', function incoming(data) {
+    console.log(data);
 });
 exports.default = server;
 //# sourceMappingURL=server.js.map
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,4 @@
+import * as WebSocket from 'ws';
 import app from "./app";
 
 const server = app.listen(app.get("port"), () => {
@@ -7,6 +8,23 @@ const server = app.listen(app.get("port"), () => {
     app.get("env")
   );
   console.log("  Press CTRL-C to stop\n");
+});
+
+//initialize the WebSocket server instance
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws: WebSocket) => {
+
+    //connection is up, let's add a simple simple event
+    ws.on('message', (message: string) => {
+
+        //log the received message and send it back to the client
+        console.log('received: %s', message);
+        ws.send(`Hello, you sent -> ${message}`);
+    });
+
+    //send immediatly a feedback to the incoming connection    
+    ws.send('Hi there, I am a WebSocket server');
 });
 
 export default server;
